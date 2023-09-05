81767a3f55acba0e88f90f4b90b0c6d8531a6e1c
Ha-JinSung <142278871+Ha-JinSung@users.noreply.github.com>
2023-09-05 12:09:42 +0900

    Add files via upload

--- js/WS_down.js
+++ js/WS_down.js
@@ -4,15 +4,13 @@ $DW_BTN.addEventListener('click', () => {
 
 function WS_Down() {
     const WS_DownDate = JSON.stringify($saved_WS, null, 1).replace(/\\"/g, "");
-    const blob = new Blob([WS_DownDate], { type: 'application/json' });
-    const $blob = new Blob([blob], { type: 'text/plain' });
+    const blob = new Blob([WS_DownDate], { type: 'text/plain' });
     const WS_DownLink = document.createElement('a');
 
-    WS_DownLink.href = URL.createObjectURL($blob);
+    WS_DownLink.href = URL.createObjectURL(blob);
     WS_DownLink.download = '명언 리스트.txt';
     WS_DownLink.style.display = 'none';
     document.body.appendChild(WS_DownLink);
     WS_DownLink.click();
-
     document.body.removeChild(WS_DownLink);
 }
